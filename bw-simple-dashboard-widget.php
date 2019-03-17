f4145e9f10206b01cbd3a42c37a36d2fc5150e34
Bowo <wibowosulistio@gmail.com>
2019-03-17 15:23:36 +0700

    Add comments

--- bw-simple-dashboard-widget.php
+++ bw-simple-dashboard-widget.php
@@ -23,13 +23,22 @@ class My_Dashboard
 
 	public function __construct() {
 
+		// Add the dashboard widget function to a hook 
 		add_action( 'wp_dashboard_setup', array( $this, 'bw_dashboard_widgets' ) );
 
 	}
 
+	/**
+	 * Output the welcome message
+	 */
+
 	function bw_dashboard_message() {
 		echo '<p>Welcome to your plugin!</p>';
 	}
+
+	/**
+	 * Add a dashboard widget that calls the function displaying te welcome message
+	 */
 
 	public function bw_dashboard_widgets() {
 	wp_add_dashboard_widget( 'personal_welcome_widget', 'Personal Welcome', array( $this, 'bw_dashboard_message' ) );
